21a713b79c6f6aaae0f8df4a6c794d25f717391e
vaschenkoda <dvaschenko33@gmail.com>
2025-12-24 13:04:12 +0300

    Fragment example

--- src/main/java/com/company/timesheets/view/client/ClientDetailView.java
+++ src/main/java/com/company/timesheets/view/client/ClientDetailView.java
@@ -1,14 +1,19 @@
 package com.company.timesheets.view.client;
 
 import com.company.timesheets.entity.Client;
+import com.company.timesheets.entity.ContactInformation;
+import com.company.timesheets.view.contactinformationfragment.ContactInformationFragment;
 import com.company.timesheets.view.main.MainView;
 import com.vaadin.flow.component.ClickEvent;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
+import io.jmix.flowui.Fragments;
+import io.jmix.flowui.component.details.JmixDetails;
 import io.jmix.flowui.component.image.JmixImage;
 import io.jmix.flowui.kit.component.button.JmixButton;
 import io.jmix.flowui.model.InstanceContainer;
 import io.jmix.flowui.view.*;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Route(value = "clients/:id", layout = MainView.class)
 @ViewController("ts_Client.detail")
@@ -18,6 +23,12 @@ public class ClientDetailView extends StandardDetailView<Client> {
 
     @ViewComponent
     private JmixImage<Object> imageDisplayField;
+//    @ViewComponent
+//    private InstanceContainer<ContactInformation> contactInformationDc;
+//    @Autowired
+//    private Fragments fragments;
+//    @ViewComponent
+//    private JmixDetails ciDetails;
 
     @Subscribe
     public void onReady(final ReadyEvent event) {
@@ -45,6 +56,17 @@ public class ClientDetailView extends StandardDetailView<Client> {
 
     private void applySecurityPermissions() {
     }
+
+//    @Subscribe
+//    public void onInit(final InitEvent event) {
+//        ContactInformationFragment fragment = fragments.create(this, ContactInformationFragment.class);
+//        fragment.setContactInformationValues(contactInformationDc);
+//        ciDetails.add(fragment);
+//    }
+    
+    
+
+
 
 
 }
